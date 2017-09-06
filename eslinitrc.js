84cfcd1bd4d0241385d00c126c6d3cfd7e716ef2
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-09-06 13:47:19 +0530

    eslint indentations changed 2 spaces with 4

--- eslinitrc.js
+++ eslinitrc.js
@@ -18,7 +18,7 @@ module.exports = {
     plugins: ['react'],
     rules: {
         'react/prop-types': ['off'],
-        indent: ['error', 2, { SwitchCase: 1 }],
+        indent: ['error', 4, { SwitchCase: 1 }],
         'linebreak-style': ['error', 'unix'],
         quotes: ['error', 'single'],
         semi: ['error', 'always'],
